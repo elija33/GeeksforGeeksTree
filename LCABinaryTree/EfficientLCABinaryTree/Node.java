e7c5d728334fa755bb7c8ea3709ddcc89e11858c
Elija33 <amponsaeli@gmail.com>
2022-10-17 22:36:46 -0400

    LCA Binary Tree

--- LCABinaryTree/EfficientLCABinaryTree/Node.java
+++ LCABinaryTree/EfficientLCABinaryTree/Node.java
@@ -1,5 +1,49 @@
 package LCABinaryTree.EfficientLCABinaryTree;
 
 public class Node {
+    int key;
+    Node right;
+    Node left;
 
+    public Node(int d) {
+        key = d;
+        right = null;
+        left = null;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+        int number1 = 20;
+        int number2 = 50;
+
+        Node num = lca(root, number1, number2);
+        System.out.println("LCA: " + num.key);
+    }
+
+    public static Node lca(Node root, int number1, int number2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.key == number1 || root.key == number2) {
+            return root;
+        }
+
+        Node lca1 = lca(root.left, number1, number2);
+        Node lca2 = lca(root.right, number1, number2);
+
+        if (lca1 != null && lca2 != null) {
+            return root;
+        }
+        if (lca1 != null) {
+            return lca1;
+        } else {
+            return lca2;
+        }
+    }
 }
--- LCABinaryTree/NavieLCA/Node.java
+++ LCABinaryTree/NavieLCA/Node.java
@@ -1,5 +1,58 @@
 package LCABinaryTree.NavieLCA;
 
-public class Node {
+import java.util.ArrayList;
 
+public class Node {
+    int key;
+    Node right;
+    Node left;
+
+    public Node(int d) {
+        key = d;
+        right = null;
+        left = null;
+    }
+}
+
+class Test {
+    public static void main(String args[]) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+        int number1 = 20, number2 = 50;
+
+        Node ans = lca(root, number1, number2);
+        System.out.println("LCA: " + ans.key);
+    }
+
+    public static boolean findPath(Node root, ArrayList<Node> name, int n) {
+        if (root == null) {
+            return false;
+        }
+        name.add(root);
+        if (root.key == n) {
+            return true;
+        }
+        if (findPath(root.left, name, n) || findPath(root.right, name, n)) {
+            return true;
+        }
+        name.remove(name.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        if (findPath(root, path1, n1) == false || findPath(root, path2, n2) == false) {
+            return null;
+        }
+        for (int i = 0; i < path1.size() - 1 && i < path2.size() - 1; i++)
+            if (path1.get(i + 1) != path2.get(i + 1)) {
+                return path1.get(i);
+            }
+        return null;
+    }
 }
