c438bdf366bdf7e67bc0f96f31a9fbcb5247414f
Elija33 <amponsaeli@gmail.com>
2022-10-21 11:59:14 -0400

    Iterative Pre Order Traversal

---
+++ IterativePreOrderTraversal/Node.java
@@ -0,0 +1,29 @@
+package IterativePreOrderTraversal;
+
+public class Node {
+    int key;
+    Node right;
+    Node left;
+
+    public Node(int d) {
+        key = d;
+        right = null;
+        left = null;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Node root = new Node(0); // root
+        root.right = new Node(1); // root -- right
+        root.left = new Node(2); // root -- left
+        root.left.left = new Node(3); // root -- left -- left
+        root.right.left = new Node(4); // root -- right -- left
+        root.left.right = new Node(5); // root -- left -- right
+        root.right.right = new Node(6); // root -- right -- right
+        root.left.left.left = new Node(7); // root -- left -- left -- left
+        root.right.right.left = new Node(8); // root -- rigth -- right -- left
+        root.left.right.right = new Node(9); // root -- left -- right -- right
+        root.right.right.right = new Node(10); // root -- right -- right -- right
+    }
+}
